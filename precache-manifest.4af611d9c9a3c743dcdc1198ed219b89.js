2c963a27db518ca138e8e170c70735f55fb88cb3
zurda <m.weizman@gmail.com>
2020-01-22 21:17:31 +0000

    Updates

---
+++ precache-manifest.4af611d9c9a3c743dcdc1198ed219b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a23961f4b761e607ba026ba40365fc39",
+    "url": "/coding-logbook/static/media/header.a23961f4.jpg"
+  },
+  {
+    "revision": "6a84a3a32b7e52771e93",
+    "url": "/coding-logbook/static/js/runtime~main.6a84a3a3.js"
+  },
+  {
+    "revision": "72b634b6f672823f65c5",
+    "url": "/coding-logbook/static/js/main.72b634b6.chunk.js"
+  },
+  {
+    "revision": "dce9370008cf3d9683f5",
+    "url": "/coding-logbook/static/js/1.dce93700.chunk.js"
+  },
+  {
+    "revision": "72b634b6f672823f65c5",
+    "url": "/coding-logbook/static/css/main.0a431f6c.chunk.css"
+  },
+  {
+    "revision": "204440baf05e2b59cbeaf975f430d057",
+    "url": "/coding-logbook/index.html"
+  }
+];
